79ff908c50ae389fe188825876b3e7916054a2b5
Karsten Schmidt <k@postspectacular.com>
2016-07-30 22:12:25 +0100

    add ditaa & plantuml jar paths to config.el

--- config.el
+++ config.el
@@ -1,5 +1,10 @@
 (setq org-html-htmlize-output-type 'css)
 
+;; these paths are for OSX homebrew installs
+;; edit as needed...
+(setq org-ditaa-jar-path "/usr/local/Cellar/ditaa/0.9/libexec/ditaa0_9.jar")
+(setq org-plantuml-jar-path "/usr/local/Cellar/plantuml/8041/plantuml.8041.jar")
+
 (org-babel-do-load-languages
  'org-babel-load-languages
  '((emacs-lisp . t)
